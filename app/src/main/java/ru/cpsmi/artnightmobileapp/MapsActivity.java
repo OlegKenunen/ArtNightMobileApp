367661aec415c3268a436633816c502a718197d8
OlegKenunen <oleg.kenunen@gmail.com>
2017-06-02 16:27:09 +0300

    Initial commit Google Maps API added

---
+++ app/src/main/java/ru/cpsmi/artnightmobileapp/MapsActivity.java
@@ -0,0 +1,57 @@
+package ru.cpsmi.artnightmobileapp;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a markers
+        LatLng museum1 = new LatLng(59.970984,30.32144);
+        mMap.addMarker(new MarkerOptions().position(museum1).title("Ботанический сад Петра Великого"));
+        LatLng museum2 = new LatLng(59.955366,30.311068);
+        mMap.addMarker(new MarkerOptions().position(museum2).title("Планетарий"));
+        LatLng museum3 = new LatLng(59.921772,30.35646);
+        mMap.addMarker(new MarkerOptions().position(museum3).title("Лофт Проект ЭТАЖИ"));
+        LatLng museum4 = new LatLng(59.965376,30.315603);
+        mMap.addMarker(new MarkerOptions().position(museum4).title("Музей интерактивной науки «ЛабиринтУм»"));
+        LatLng museum5 = new LatLng(59.958073,30.317481);
+        mMap.addMarker(new MarkerOptions().position(museum5).title("Ленфильм"));
+
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(museum5));
+        mMap.setMinZoomPreference(10.0f);
+    }
+}
