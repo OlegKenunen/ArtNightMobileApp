8ea814fd4f282916a357e10455aac4f10759154e
OlegKenunen <oleg.kenunen@gmail.com>
2017-06-02 17:21:21 +0300

    add MyLocation point

--- app/src/main/java/ru/cpsmi/artnightmobileapp/MapsActivity.java
+++ app/src/main/java/ru/cpsmi/artnightmobileapp/MapsActivity.java
@@ -1,5 +1,7 @@
 package ru.cpsmi.artnightmobileapp;
 
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -53,5 +55,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(museum5));
         mMap.setMinZoomPreference(10.0f);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+
     }
 }
